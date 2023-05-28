cbb263fc1d2db60055f623e81a9d9452e0931b83
Zachary Juang <zachary822@me.com>
2023-05-28 02:02:43 -0400

    render posts

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,6 @@ import Control.Monad
 import Control.Monad.Catch
 import Control.Monad.IO.Class
 import Control.Monad.State qualified as MS
-import Control.Monad.Trans.State
 import Control.Monad.Trans.State qualified as S
 import Crypto.Random
 import Data.Aeson hiding (Success)
@@ -30,20 +29,29 @@ import Brick
 import Brick.AttrMap qualified as A
 import Brick.Widgets.Border qualified as B
 import Brick.Widgets.List qualified as L
+import Control.Applicative
+import Data.Either (fromRight)
+import Data.Maybe (fromMaybe)
 import Data.Vector qualified as Vec
 import Graphics.Vty qualified as V
 import Graphics.Vty.Input.Events
+import Text.Pandoc.Class
+import Text.Pandoc.Error (PandocError)
+import Text.Pandoc.Options (def)
+import Text.Pandoc.Readers.HTML
+import Text.Pandoc.Writers.Markdown
 
 import Control.Monad.IO.Class
 import Debug.Trace
 
-data Name = Posts | SBClick ClickableScrollbarElement Name deriving (Eq, Ord, Show)
+data Name = Posts | Post | Comments deriving (Eq, Ord, Show)
 type LinkList = L.List Name Link
 
 data AppState = AppState
   { cursorState :: Cursor String
   , listState :: LinkList
   , tokenState :: String
+  , focusedName :: Name
   }
 
 notSymbol :: Char -> Bool
@@ -54,27 +62,55 @@ linkWidget l = case destUrl l of
   Nothing -> w
   Just u -> hyperlink (T.pack u) w
  where
-  w = txtWrap . T.filter notSymbol $ "* " <> title l
+  w = txt "* " <+> str ("(/r/" <> subreddit l <> ") ") <+> (txt . T.filter notSymbol $ title l)
+
+renderPost :: T.Text -> Either PandocError T.Text
+renderPost e = runPure $ readHtml def e >>= writeMarkdown def
+
+padPostContent :: Widget n -> Widget n
+padPostContent = padLeft (Pad 1) . padRight Max . padTop (Pad 1) . padBottom (Pad 1)
+
+renderFocused :: Bool -> Widget n -> Widget n
+renderFocused focused w =
+  if focused then withAttr (attrName "focused") w else w
 
 drawUI :: AppState -> [Widget Name]
-drawUI l = [postLayer, ui]
+drawUI st = [postLayer, ui]
  where
   label = txt "Item " <+> cur <+> txt " of " <+> total
-  cur = case L.listSelected $ listState l of
+  cur = case L.listSelected $ listState st of
     Nothing -> txt "-"
     Just i -> str (show $ i + 1)
-  total = str . show . Vec.length . L.listElements $ listState l
+  total = str . show . Vec.length . L.listElements $ listState st
+  fn = focusedName st
   box =
-    reportExtent Posts $
-      B.borderWithLabel label $
-        withVScrollBars OnRight $
-          L.renderList listDrawElement True $
-            listState l
+    B.borderWithLabel label $
+      withVScrollBars OnRight $
+        L.renderList listDrawElement (fn == Posts) $
+          listState st
   helpText = txt "Press Q to exit"
   ui = vBox [box, helpText]
-
-  -- postLayer = relativeTo Posts (Location (2, 1)) $ B.border $ txt "yay"
-  postLayer = relativeTo Posts (Location (2, 1)) emptyWidget
+  postLayer =
+    if fn == Post || fn == Comments
+      then
+        ( case L.listSelectedElement (listState st) of
+            Nothing -> emptyWidget
+            Just (_, e) ->
+              withVScrollBars OnRight . joinBorders $
+                vBox
+                  [ ( B.borderWithLabel
+                        (renderFocused (fn == Post) (txt . T.filter notSymbol . title $ e))
+                        $ viewport Post Vertical
+                        $ (txtWrap $ fromMaybe (T.pack $ url e) ((fromRight "" . renderPost <$> selfText e) <|> (T.pack <$> destUrl e)))
+                    )
+                  , ( B.borderWithLabel
+                        (renderFocused (fn == Comments) (txt "Comments"))
+                        $ viewport Comments Vertical
+                        $ txt "yay!"
+                    )
+                  ]
+        )
+      else emptyWidget
 
 listDrawElement :: Bool -> Link -> Widget Name
 listDrawElement sel a =
@@ -86,7 +122,7 @@ listDrawElement sel a =
 loadNextPage :: Event -> EventM Name AppState ()
 loadNextPage e = do
   st <- MS.get
-  nl <- nestEventM' (listState st) $ L.handleListEvent e
+  nl <- nestEventM' (listState st) $ L.handleListEventVi L.handleListEvent e
   let total = Vec.length . L.listElements $ nl
   let curr = L.listSelected nl
   let end = case curr of
@@ -95,7 +131,7 @@ loadNextPage e = do
   if end
     then
       ( do
-          (posts, cursor) <- runStateT (getNextPosts $ tokenState st) (cursorState st)
+          (posts, cursor) <- S.runStateT (getNextPosts $ tokenState st) (cursorState st)
           MS.put
             st
               { listState = (L.listReplace (L.listElements nl <> Vec.fromList posts) curr nl)
@@ -105,12 +141,39 @@ loadNextPage e = do
     else MS.put st{listState = nl}
 
 appEvent :: BrickEvent Name e -> EventM Name AppState ()
-appEvent (VtyEvent e) =
+appEvent (VtyEvent e) = do
+  st <- MS.get
   case e of
     EvKey (KChar 'q') [] -> halt
+    EvKey KEsc [] -> MS.put (st{focusedName = Posts})
+    EvKey key [] -> do
+      let fn = focusedName st
+      -- TODO scrolling is scuffed
+      case fn of
+        Posts ->
+          ( case key of
+              KEnter -> (MS.put (st{focusedName = Post}))
+              _ -> loadNextPage e
+          )
+        Post ->
+          ( case key of
+              KChar '\t' -> (MS.put (st{focusedName = Comments}))
+              KUp -> vScrollBy (viewportScroll fn) (-1)
+              KDown -> vScrollBy (viewportScroll fn) 1
+              KPageUp -> vScrollPage (viewportScroll fn) Up
+              KPageDown -> vScrollPage (viewportScroll fn) Down
+              _ -> return ()
+          )
+        Comments ->
+          ( case key of
+              KChar '\t' -> (MS.put (st{focusedName = Post}))
+              KUp -> vScrollBy (viewportScroll fn) (-1)
+              KDown -> vScrollBy (viewportScroll fn) 1
+              KPageUp -> vScrollPage (viewportScroll fn) Up
+              KPageDown -> vScrollPage (viewportScroll fn) Down
+              _ -> return ()
+          )
     _ -> loadNextPage e
-
--- (posts, cursor) <- runStateT (getNextPosts $ tokenState st) (cursorState st)
 appEvent _ = return ()
 
 customApp :: App AppState e Name
@@ -130,6 +193,7 @@ theMap =
   A.attrMap
     V.defAttr
     [ (L.listSelectedAttr, V.black `on` V.white)
+    , (attrName "focused", V.black `on` V.white)
     ]
 
 oauth :: Oauth2
@@ -144,7 +208,7 @@ oauth =
 getToken :: FilePath -> IO (Either String RedditToken)
 getToken p = eitherDecodeFileStrict' p
 
-getNextPosts :: (MonadThrow m, MonadIO m) => String -> StateT (Cursor String) m [Link]
+getNextPosts :: (MonadThrow m, MonadIO m) => String -> S.StateT (Cursor String) m [Link]
 getNextPosts token = do
   cursor <- S.get
   resp <- redditGetEndpoint token "/" cursor
@@ -164,13 +228,14 @@ main = do
     Right token -> do
       t <- access_token <$> redditAccessToken oauth (refresh_token token)
 
-      (posts, cursor) <- runStateT (getNextPosts t) NoCursor
+      (posts, cursor) <- S.runStateT (getNextPosts t) NoCursor
 
       let initialState =
             AppState
               { listState = (L.list Posts (Vec.fromList posts) 1)
               , cursorState = cursor
               , tokenState = t
+              , focusedName = Posts
               }
 
       void $ defaultMain customApp initialState
